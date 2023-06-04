d6eb9fd97b4f487ce42919dac10a989da0d2d7bb
mborodin <m.borodin.1999@gmail.com>
2023-06-04 16:35:07 +0200

    updated images

--- postgres-config/01-create-testsystem-db.sql
+++ postgres-config/01-create-testsystem-db.sql
@@ -1,4 +1,4 @@
 CREATE USER testsystem_user;
 ALTER USER testsystem_user WITH PASSWORD 'password';
 CREATE DATABASE testsystem;
-GRANT ALL PRIVILEGES ON DATABASE testsystem TO testsystem_user;
+ALTER DATABASE testsystem OWNER TO testsystem_user;
--- postgres-config/03-create-uwithme-db.sql
+++ postgres-config/03-create-uwithme-db.sql
@@ -1,4 +1,4 @@
 CREATE USER uwithme_user;
 ALTER USER uwithme_user WITH PASSWORD 'password';
 CREATE DATABASE uwithme;
-GRANT ALL PRIVILEGES ON DATABASE uwithme TO uwithme_user;
+ALTER DATABASE uwithme OWNER TO uwithme_user;
